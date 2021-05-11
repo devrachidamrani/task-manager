1802d8703f584d536d0b35d38f37c4b69aa692f9
Rachid Amrani <rachid3alaoui@gmail.com>
2021-05-11 14:34:31 +0000

    add states and handlers

--- app/App.js
+++ app/App.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TaskForm from '../components/TaskForm'
 import TasksList from '../components/TasksList'
 
 const App = () => {
+  const [newTask, setNewTask] = useState({})
+  const [allTasks, setAllTasks] = useState([])
+
+  const addNewTask = () => {
+    if (!newTask.task) alert('Please fill in the input')
+    setAllTasks((prevState) => {
+      return [...prevState, newTask]
+    })
+  }
+
+  const deleteTask = (id) => {}
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target
+    setNewTask((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+        id: Date.now(),
+      }
+    })
+  }
+
   return (
     <div className="app">
       <h1> Task Manager </h1>
+
       <hr />
-      <TaskForm />
-      <TasksList />
+      <TaskForm onChange={handleChange} onAdd={addNewTask} />
+      <TasksList onDelete={deleteTask} allTasks={allTasks} />
     </div>
   )
 }
