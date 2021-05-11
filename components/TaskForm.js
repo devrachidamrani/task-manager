d8e90d9fb52d32b739f8e18d9ccd5a0954a5d9a6
Rachid Amrani <rachid3alaoui@gmail.com>
2021-05-11 14:12:37 +0000

    Add components/ folder

---
+++ components/TaskForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TaskForm = () => {
+  return (
+    <>
+      <input type="text" placeholder="Enter task here .." />{' '}
+      <button variant="text" color="default">
+        Save
+      </button>
+    </>
+  )
+}
+
+export default TaskForm
---
+++ components/TasksList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TasksList = () => {
+  return (
+    <div>
+      <h2>Tasks List</h2>
+    </div>
+  )
+}
+
+export default TasksList
