aaa9513cb9a31d3cedf1b083493060c4334b21c6
Rachid Amrani <rachid3alaoui@gmail.com>
2021-05-11 14:11:52 +0000

    Apply css styles using app classname

--- app/App.js
+++ app/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import TaskForm from '../components/TaskForm'
+import TasksList from '../components/TasksList'
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <h1> Task Manager </h1>
+      <hr />
+      <TaskForm />
+      <TasksList />
     </div>
   )
 }
